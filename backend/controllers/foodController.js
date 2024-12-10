b70a378f0ace7d2b68895e0e97514c048b333734
RITU KANSAL <87207504+ritu456286@users.noreply.github.com>
2024-12-10 16:33:50 +0530

    completed basic routes in backend, set up admin

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -3,7 +3,6 @@ import fs from 'fs'
 
 
 //add food item
-
 const addFood = async (req, res) => {
     let image_filename =  `${req.file.filename}`;
     const food = new foodModel({
@@ -23,4 +22,30 @@ const addFood = async (req, res) => {
     }
 }
 
-export {addFood}
+
+// list all food items
+const listFood = async(req, res) => {
+    try {
+        const foods = await foodModel.find({});
+        res.json({success:true, data:foods});
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error});
+    }
+}
+
+//remove food items
+const removeFood = async(req, res) => {
+    try {
+      
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`, () =>{});
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({success:true, message: "Food removed!"})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "Error"});
+    }
+}
+
+export {addFood, listFood, removeFood}
--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { addFood } from "../controllers/foodController.js"
+import { addFood, listFood, removeFood } from "../controllers/foodController.js"
 import multer from "multer"
 
 const foodRouter = express.Router();
@@ -14,7 +14,14 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage:storage})
 
-
+/**POST: Add Food Item */
 foodRouter.post("/add", upload.single("image"), addFood)
+
+/**GET: List all Food Items */
+foodRouter.get('/list', listFood)
+
+/**POST: Remove Food Item */
+foodRouter.post("/remove", removeFood)
+
 
 export default foodRouter;
